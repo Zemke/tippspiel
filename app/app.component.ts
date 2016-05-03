707af80fe43125b209399dda939143147ffae04d
Zemke <flzemke@gmail.com>
2016-05-03 18:20:09 +0200

    Add routing and Material Design Lite

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,13 @@
 import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES, RouteConfig} from "angular2/router";
+import {RegistrationComponent} from "./registration.component";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    {path: '/registration', name: 'Registration', component: RegistrationComponent}
+])
 export class AppComponent { }
---
+++ app/registration.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: '<soe-registration></soe-registration>',
+  template: 'Hello, World!'
+})
+export class RegistrationComponent {}
