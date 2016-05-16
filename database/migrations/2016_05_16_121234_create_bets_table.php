52b13bcf11c646793683545184469a426f09601f
Zemke <flzemke@gmail.com>
2016-05-16 14:39:13 +0200

    #11 Exchange todos for bets

---
+++ database/migrations/2016_05_16_121234_create_bets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bets', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('home_goals');
+            $table->integer('away_goals');
+            $table->string('valuation');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bets');
+    }
+}
