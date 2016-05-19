3cc6e0c05b30f37fedd6f404042431727ab764dd
Zemke <flzemke@gmail.com>
2016-05-20 00:57:38 +0200

    #11 Set composite primary key

--- database/migrations/2016_05_16_121234_create_bets_table.php
+++ database/migrations/2016_05_16_121234_create_bets_table.php
@@ -14,12 +14,13 @@ class CreateBetsTable extends Migration
     {
         Schema::create('bets', function(Blueprint $table)
         {
-            $table->increments('id');
             $table->integer('home_goals');
             $table->integer('away_goals');
+            $table->integer('fixture_id');
             $table->string('valuation');
             $table->integer('user_id');
             $table->timestamps();
+            $table->primary(['fixture_id', 'user_id']);
         });
     }
 
