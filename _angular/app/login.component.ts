0d0e92b463a16a8f2fb9e8899df2d961d5b775b2
Zemke <flzemke@gmail.com>
2016-06-07 01:56:52 +0200

    Use Toasts for login, too

--- _angular/app/login.component.ts
+++ _angular/app/login.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-import {TranslatePipe} from 'ng2-translate/ng2-translate';
+import {TranslatePipe, TranslateService} from 'ng2-translate/ng2-translate';
 import {User} from './user';
 import {MDL} from './material-design-lite-upgrade-element.directive';
 import {UserService} from './user.service';
+import {ToastsManager} from 'ng2-toastr/ng2-toastr';
 
 @Component({
   selector: '<soe-login></soe-login>',
@@ -14,13 +15,15 @@ export class LoginComponent {
   user:User = new User(1, '', '', '', '', '', '');
   error:string;
 
-  constructor(private userService:UserService) {
+  constructor(private userService:UserService, private toastr:ToastsManager, private translateService:TranslateService) {
   }
 
   onSubmit() {
     this.userService.login(this.user.email, this.user.password)
         .subscribe(
             response => {localStorage.setItem('user_token', response.token); location.href = '/'},
-            error => this.error = error);
+            error => this.toastr.error(
+                this.translateService.instant('soe.toast.login.failed'),
+                this.translateService.instant('soe.toast.failed')));
   }
 }
