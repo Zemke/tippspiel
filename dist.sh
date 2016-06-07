6546f5ba72c69a1a1231318a0c012a5d7c06e91a
Zemke <flzemke@gmail.com>
2016-06-07 03:42:42 +0200

    #14 Shell to automate Laravel integration

---
+++ dist.sh
@@ -0,0 +1,15 @@
+cd _angular
+npm install
+npm run-script tsc
+
+cd ../
+
+cp -r _angular/node_modules public/node_modules
+cp -r _angular/i18n/ public/i18n
+
+cp _angular/styles.css public/
+cp _angular/systemjs.config.js public/
+
+mkdir public/app
+cp _angular/app/*.js public/app/
+cp _angular/app/*.html public/app/
