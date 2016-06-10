82187463a55cb3974885897ef6f8bd9a4c4fc868
Zemke <flzemke@gmail.com>
2016-06-11 00:02:10 +0200

    Bring beginning page from `_angular`

--- dist.sh
+++ dist.sh
@@ -4,6 +4,7 @@ npm run-script tsc
 
 cd ../
 
+rm -r public/node_modules
 cp -r _angular/node_modules public/node_modules
 cp -r _angular/i18n/ public/i18n
 
@@ -13,5 +14,8 @@ cp _angular/systemjs.config.js public/
 mkdir -p public/app
 cp _angular/app/*.js public/app/
 cp _angular/app/*.html public/app/
+
+mkdir -p resources/views
+cp _angular/index.html resources/views/layout.blade.php
 
 echo "Done."
