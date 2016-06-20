e23787897247755b6dfcafdcb787b32b18722ccf
Zemke <flzemke@gmail.com>
2016-06-21 00:04:20 +0200

    #9 Add column for champ bet

---
+++ database/migrations/2016_06_20_210009_add_team_id_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTeamIdToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('champ_bet');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('champ_bet');
+        });
+    }
+}
